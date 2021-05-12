9a2345c437dd8585905dd9fd84a3b4ab93b9d754
Fernando Fernandes <fernando.fernandes@labtwin.com>
2021-05-12 11:02:16 +0200

    Documentation

--- Sources/ALMA/ALMA+Protocol.swift
+++ Sources/ALMA/ALMA+Protocol.swift
@@ -4,6 +4,6 @@ public protocol ALMAProtocol {
 
     /// Enables or disables logging.
     ///
-    /// - Parameter enabled: `true` if `ALMA` should output log info; `false` otherwise
+    /// - Parameter enabled: `true` if `ALMA` should output log info; `false` otherwise.
     func setLogging(enabled: Bool)
 }
